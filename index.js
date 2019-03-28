ddfedad1e0c4bf90a0e493483176c709f7d64d2a
Family <sabziamjad@gmail.com>
2019-03-28 22:33:45 +0000

    added changes to package.json

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
 app.use(express.static(`${__dirname}/public`));
-app.use(express.static('public'));
+
 
 app.use(expressLayouts);
 
